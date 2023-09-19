b7a35fef50f3d9779754c61e33e1209856cbb081
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-09-19 17:56:24 +0900

    create app/(auth)/login/page.tsx

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,32 @@
+import AuthForm from '@/app/(auth)/components/AuthForm'
+import Image from 'next/image'
+import discordLogo from '@/public/images/discord-logo-white.svg'
+
+const LoginPage = () => {
+  return (
+    <div className="flex w-full justify-center md:grid-cols-2 md:gap-0 md:rounded-lg md:mx-4">
+      {/* right side logo space */}
+      <div
+        className="hidden md:flex md:flex-col md:w-full md:items-center md:justify-center 
+        md:max-w-md md:px-8 md:py-12 md:bg-gray-500 md:rounded-tl-lg md:rounded-bl-lg"
+      >
+        <Image src={discordLogo} alt="logo" className="py-8" />
+        <h1 className="text-5xl font-bold text-white">Clone</h1>
+      </div>
+      {/* left side form space */}
+      <div className="w-full max-w-md px-8 py-12 bg-gray-100 rounded-md md:rounded-none shadow md:rounded-tr-lg md:rounded-br-lg">
+        <div className="">
+          <h2
+            className="text-center text-2xl font-bold
+          leading-9 tracking-tight text-gray-900"
+          >
+            Welcome back!
+          </h2>
+        </div>
+        <AuthForm />
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
