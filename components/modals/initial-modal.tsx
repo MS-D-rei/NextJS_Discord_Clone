c28757917c94db3fe7fdf50fcbf4a80e3ed73b55
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-10-06 09:26:03 +0900

    Add: Form for server name to components/modals/initial-modal.tsx

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -13,20 +13,40 @@ import {
   DialogDescription,
   DialogFooter,
 } from '@/components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 const formSchema = z.object({
-  serverName: z.string().min(3, { message: 'Server name must be at least 3 characters' }),
+  serverName: z
+    .string()
+    .min(3, { message: 'Server name must be at least 3 characters' }),
   serverImage: z.string().url({ message: 'Server image must be a valid URL' }),
 })
 
 const InitialModal: React.FC = () => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       serverName: '',
       serverImage: '',
-    }
+    },
   })
+
+  if (!isMounted) return null
 
   const onSubmit = (data: z.infer<typeof formSchema>) => {
     console.log('isSubmitting', form.formState.isSubmitting)
@@ -45,6 +65,44 @@ const InitialModal: React.FC = () => {
             always change it later.
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="px-6 pb-8">
+              <div>TODO: Image upload</div>
+              <FormField
+                control={form.control}
+                name="serverName"
+                render={({ field, formState }) => (
+                  <FormItem>
+                    <FormLabel
+                      htmlFor="serverName"
+                      className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70 pl-2"
+                    >
+                      Server Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={form.formState.isSubmitting}
+                        placeholder="Enter server name"
+                        className={`bg-zinc-300/50 text-black border-0
+                        focus-visible:ring-0 focus-visible:ring-offset-0`}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage>
+                      {formState.errors.serverName?.message}
+                    </FormMessage>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button disabled={form.formState.isSubmitting} variant="primary">
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   )
