bbc48daf681dac483e79ff40c5a69143cd5ae336
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-10-07 16:26:46 +0900

    Add: logic to set serverImage value of form in components/ImageUpload.tsx
    1. use useFormContext and this enable to remove props from initial-modal.tsx
    2. change DOM structure
    3. add handleInputChange func for upload image procedure and set value to form.

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,49 +1,102 @@
-import { ChangeEvent, ComponentPropsWithRef, useRef } from 'react'
-import { Button } from '@/components/ui/button'
+'use client'
+
+import { ChangeEvent, useRef, useState } from 'react'
 import Image from 'next/image'
+import { UseFormReturn, useFormContext } from 'react-hook-form'
 
-interface ImageUploadProps extends ComponentPropsWithRef<'div'> {
-  selectedFile?: string
-  setSelectedFile: (value: string) => void
-  handleUploadImage: (event: ChangeEvent<HTMLInputElement>) => void
-}
+import { Button } from '@/components/ui/button'
+import { FormControl, FormField, FormItem } from '@/components/ui/form'
 
-const ImageUpload: React.FC<ImageUploadProps> = ({
-  selectedFile,
-  setSelectedFile,
-  handleUploadImage,
-}) => {
+const ImageUpload: React.FC = () => {
+  // need to show the image preview
+  const [selectedFile, setSelectedFile] = useState<string>()
+  // need to invoke the hidden input
   const selectedFileRef = useRef<HTMLInputElement>(null)
+
+  // can use useFormContext() to get the form context
+  const form: UseFormReturn<
+    { serverName: string; serverImage: string },
+    any,
+    undefined
+  > = useFormContext()
 
   const handleUploadClick = () => {
     selectedFileRef.current?.click()
   }
 
-  const handleRemoveClick = () => {
-    setSelectedFile('')
+  // https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader()
+
+    if (event.target.files?.[0]) {
+      reader.readAsDataURL(event.target.files[0])
+    }
+
+    reader.onload = (readerEvent) => {
+      if (readerEvent.target?.result) {
+        setSelectedFile(readerEvent.target.result as string)
+        form.setValue('serverImage', readerEvent.target.result as string)
+      }
+    }
   }
 
+  const handleRemoveClick = () => {
+    setSelectedFile('')
+    form.setValue('serverImage', '')
+  }
+
+  // https://react-hook-form.com/faqs#Howtosharerefusage
+  // to avoid overwrite the ref of react hook form,
+  // we need to spread the ref to the input element.
+
+  // https://react.dev/learn/manipulating-the-dom-with-refs#how-to-manage-a-list-of-refs-using-a-ref-callback
+
+  // react-hook-form value attribute should be removed.
+  // otherwise, form.setValue() will try to update the value of input element.
+  // this will cause the error below:
+  /* InvalidStateError: Failed to set the 'value' property on 'HTMLInputElement'
+   * : This input element accepts a filename,
+   * which may only be programmatically set to the empty string. */
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#value
   return (
     <div className="flex items-center justify-center">
+      <FormField
+        control={form.control}
+        name="serverImage"
+        render={({ field: { ref, onChange, value, ...rest }, formState }) => (
+          <FormItem>
+            <FormControl>
+              <input
+                {...rest}
+                type="file"
+                hidden
+                ref={(e) => {
+                  ref(e)
+                  selectedFileRef.current = e
+                }}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
       {selectedFile ? (
-        <div>
+        <div className="flex flex-col items-center space-y-4">
           <Image
             src={selectedFile}
-            width={400}
-            height={400}
+            width={200}
+            height={200}
             alt="server image"
           />
-          <Button onClick={handleRemoveClick}>Remove</Button>
+          <Button type="button" onClick={handleRemoveClick}>
+            Remove
+          </Button>
         </div>
       ) : (
         <div>
-          <Button onClick={handleUploadClick}>Upload</Button>
-          <input
-            type="file"
-            ref={selectedFileRef}
-            hidden={true}
-            onChange={handleUploadImage}
-          />
+          <Button type="button" onClick={handleUploadClick}>
+            Upload
+          </Button>
         </div>
       )}
     </div>
