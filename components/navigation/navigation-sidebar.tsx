96899493f08de351a3268f044105ac7d691c6284
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-10-12 13:29:57 +0900

    Create: components/navigation/navigation-sidebar.tsx to show server icons list

---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,35 @@
+import { redirect } from 'next/navigation'
+
+import prismaClient from '@/lib/prisma-client'
+import { useCurrentUser } from '@/app/_helper/useCurrentUser'
+import { Separator } from '@/components/ui/separator'
+
+const NavigationSidebar: React.FC = async () => {
+  const currentUser = await useCurrentUser()
+
+  if (!currentUser) {
+    return redirect('/login')
+  }
+
+  // get all servers that the current user is a member of
+  const server = await prismaClient.server.findMany({
+    where: {
+      members: {
+        some: {
+          userId: currentUser.id,
+        },
+      },
+    },
+  })
+
+  return (
+    <div className="flex flex-col h-full w-full items-center text-primary bg-[#E3E5E8] py-3">
+      {/* TODO: NavigationSidebar */}
+      {/* <NavigationAction /> */}
+      <div>sidebar</div>
+      <Separator orientation='horizontal' className="h-[2px] w-10 bg-zinc-300 rounded-md mx-auto" />
+    </div>
+  )
+}
+
+export default NavigationSidebar
