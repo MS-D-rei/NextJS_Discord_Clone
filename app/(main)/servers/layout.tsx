b7906c52a416353f23bc0d9cb59e3ad4d4f3b356
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-10-12 13:30:53 +0900

    Add: NavigationSidebar component to app/(main)/servers/layout.tsx

--- app/(main)/servers/layout.tsx
+++ app/(main)/servers/layout.tsx
@@ -1,15 +1,14 @@
 import { PropsWithChildren } from 'react'
 
+import NavigationSidebar from '@/components/navigation/navigation-sidebar'
+
 const ServerIdLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className='h-full'>
+    <div className="h-full">
       <div className="hidden md:flex flex-col h-full w-[72px] z-30 fixed inset-y-0">
-        {/* TODO: NavigationSidebar */}
-        {/* <NavigationSidebar /> */}
+        <NavigationSidebar />
       </div>
-      <main className='h-full md:pl-[72px]'>
-        {children}
-      </main>
+      <main className="h-full md:pl-[72px]">{children}</main>
     </div>
   )
 }
