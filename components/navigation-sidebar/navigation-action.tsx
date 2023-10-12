9852e0fb68fea12fd5aad3d4cf609496a561e09e
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-10-12 16:35:07 +0900

    Add: ActionTooltip component to components/navigation-sidebar/navigation-action.tsx

--- components/navigation-sidebar/navigation-action.tsx
+++ components/navigation-sidebar/navigation-action.tsx
@@ -1,20 +1,22 @@
 import { Plus } from 'lucide-react'
+import ActionTooltip from '../action-tooltip'
 
 const NavigationAction: React.FC = () => {
   return (
-    // TODO: wrap entire by ActionTooltip
     <div>
-      <button type="button" className="group flex items-center">
-        <div
-          className="flex items-center justify-center h-[48px] w-[48px] rounded-[24px]
+      <ActionTooltip label="Create new server" side="right" align="center">
+        <button type="button" className="group flex items-center">
+          <div
+            className="flex items-center justify-center h-[48px] w-[48px] rounded-[24px]
           group-hover:rounded-[16px] transition-all overflow-hidden bg-background group-hover:bg-emerald-500 mx-3"
-        >
-          <Plus
-            size={24}
-            className="transition text-emerald-500 group-hover:text-white"
-          />
-        </div>
-      </button>
+          >
+            <Plus
+              size={24}
+              className="transition text-emerald-500 group-hover:text-white"
+            />
+          </div>
+        </button>
+      </ActionTooltip>
     </div>
   )
 }
