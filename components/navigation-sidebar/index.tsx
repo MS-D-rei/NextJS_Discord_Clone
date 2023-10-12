d54126d9ce4da9c20712caf40837bea5e507114c
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-10-12 17:03:22 +0900

    Add: ScrollArea component to components/navigation-sidebar/index.tsx

--- components/navigation-sidebar/index.tsx
+++ components/navigation-sidebar/index.tsx
@@ -2,8 +2,9 @@ import { redirect } from 'next/navigation'
 
 import prismaClient from '@/lib/prisma-client'
 import { useCurrentUser } from '@/app/_helper/useCurrentUser'
-import { Separator } from '@/components/ui/separator'
 import NavigationAction from '@/components/navigation-sidebar/navigation-action'
+import { Separator } from '@/components/ui/separator'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 const NavigationSidebar: React.FC = async () => {
   const currentUser = await useCurrentUser()
@@ -13,7 +14,7 @@ const NavigationSidebar: React.FC = async () => {
   }
 
   // get all servers that the current user is a member of
-  const server = await prismaClient.server.findMany({
+  const servers = await prismaClient.server.findMany({
     where: {
       members: {
         some: {
@@ -24,10 +25,18 @@ const NavigationSidebar: React.FC = async () => {
   })
 
   return (
-    <div className="flex flex-col h-full w-full items-center text-primary bg-[#E3E5E8] py-3">
+    <div className="flex flex-col h-full w-full items-center text-primary bg-[#E3E5E8] space-y-4 py-3">
       <NavigationAction />
-      <div>sidebar</div>
-      <Separator orientation='horizontal' className="h-[2px] w-10 bg-zinc-300 rounded-md mx-auto" />
+      <Separator
+        orientation="horizontal"
+        className="h-[2px] w-10 bg-zinc-300 rounded-md mx-auto"
+      />
+      <ScrollArea className='flex-1 w-full'>
+        {/* TODO: add server item to show each server that the user is a member of */}
+        {servers.map((server) => (
+          <div key={server.id}>{server.name}</div>
+        ))}
+      </ScrollArea>
     </div>
   )
 }
